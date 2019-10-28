1a02f00e9f86f22fdb5bebec9d34371d5cb54ce5
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2019-10-28 13:05:34 -0600

    added WorkLog, and got NumberSet to pass tests

--- src/Card.py
+++ src/Card.py
@@ -5,18 +5,21 @@ import NumberSet
 class Card():
     def __init__(self, idnum, size, numberSet):
         """Card constructor"""
+        self.idNum = idnum
+        self.size = size
+        self.numberSet = numberSet
         pass
 
 
     def getId(self):
         """Return an integer: the ID number of the card"""
-        pass
+        return self.idNum
 
     def getSize(self):
         """Return an integer: the size of one dimension of the card.
         A 3x3 card will return 3, a 5x5 card will return 5, etc.
         """
-        pass
+        return self.size
 
     def print(self, file=sys.stdout):
         """void function:
--- src/NumberSet.py
+++ src/NumberSet.py
@@ -1,25 +1,42 @@
 import random
 
+
 class NumberSet():
     def __init__(self, size):
         """NumberSet constructor"""
-        pass
+        self.size = size
+        self.numberSet = []
+        self.currIndex = 0
+        i = 1
+
+        while i <= self.size:
+            self.numberSet.append(i)
+            i += 1
 
     def getSize(self):
         """Return an integer: the size of the NumberSet"""
-        pass
+        return self.size
 
     def get(self, index):
         """Return an integer: get the number from this NumberSet at an index"""
-        pass
+        if self.size <= 0:
+            return None
 
+        return self.numberSet[index]
 
     def randomize(self):
         """void function: Shuffle this NumberSet"""
-        pass
-
+        random.shuffle(self.numberSet)
 
     def getNext(self):
         """Return an integer: when called repeatedly return successive values
         from the NumberSet until the end is reached, at which time 'None' is returned"""
-        pass
+        if self.size <= 0:
+            return None
+
+        if self.currIndex >= self.size:
+            self.currIndex = 0
+
+        oldIndex = self.currIndex
+        self.currIndex += 1
+        return self.numberSet[oldIndex]
